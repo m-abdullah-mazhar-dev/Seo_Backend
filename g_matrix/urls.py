8e19fed85638286c3ca96a07799426f12a59667c
Abdullah <mabdullahmazhar232@gmail.com>
2025-08-08 02:31:01 +0500

    google analytics added

--- g_matrix/urls.py
+++ g_matrix/urls.py
@@ -1,10 +1,14 @@
 # search_console/urls.py
 
 from django.urls import path
-from g_matrix.views import AuthStartView, AuthCallbackView, SyncKeywordMetricsView
+from g_matrix.views import *
 
 urlpatterns = [
     path('auth/start/', AuthStartView.as_view(), name='search_console_auth_start'),
     path('oauth2callback/', AuthCallbackView.as_view(), name='search_console_auth_callback'),
     path('sync/', SyncKeywordMetricsView.as_view(), name='search_console_sync'),
+
+    path('analytics/auth/start/', google_analytics_auth_start, name='ga_auth_start'),
+    path('analytics/oauth2callback/', analytics_oauth2callback, name='ga_auth_callback'),
+    path('analytics/fetch-data/', fetch_analytics_data, name='ga_fetch_data'),
 ]
