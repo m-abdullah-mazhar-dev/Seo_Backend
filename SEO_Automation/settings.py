771f443a87f8ccd521b4625b6b1e4899932761c5
raohassanbotmer <rao.hasan@botmer.io>
2025-08-27 14:53:06 +0500

    cors

--- SEO_Automation/settings.py
+++ SEO_Automation/settings.py
@@ -199,13 +199,13 @@ SIMPLE_JWT = {
 
 
 # 6
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8000",
-#     "http://localhost:3000",
-#     "http://127.0.0.1:9000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOW_METHODS = [
