1d6e37e40cbfa6319c72b452cc14299b6d820887
Abdullah <mabdullahmazhar232@gmail.com>
2025-08-28 20:16:24 +0500

    seperated job wordpress api

--- seo_services/urls.py
+++ seo_services/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('onboarding/', OnBoardingFormAPIView.as_view(), name= "onboarding"),
     path('onboarding/<int:pk>/', OnBoardingFormAPIView.as_view()),
     path('connect_wp/', ConnectWordPressAPI.as_view()),
+    path('connect_wp/job/', ConnectWordPressAPIJob.as_view()),
     path('verify_wp_connection/', VerifyWordPressConnectionAPI.as_view()),
     path('submit_service_page/', SubmitServicePageAPI.as_view()),
 
